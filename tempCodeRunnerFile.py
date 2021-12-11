1aff771ae7c755e4418f9b640d724bbececb847a
Jackie <jramsey@zagmail.gonzaga.edu>
2021-12-11 15:25:36 -0800

    update commit

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-x1 = flying
-y1 = list(range(len(x1)))
-plt.scatter(x1, y1)
-plt.show()
---
+++ utils.py
@@ -0,0 +1,6 @@
+from numpy.core.fromnumeric import mean
+import pandas as pd
+from pandas.io.parsers import read_csv
+import numpy as np
+import matplotlib.pyplot as plt 
+import scipy.stats as stats
