ff98461cf03550dde8b0359e13596046714d8d23
jackieaiko <71041342+jackieaiko@users.noreply.github.com>
2021-12-15 10:08:09 -0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -4,3 +4,6 @@ from pandas.io.parsers import read_csv
 import numpy as np
 import matplotlib.pyplot as plt 
 import scipy.stats as stats
+
+# any useful functions to be implemented in technical_report.ipynb
+# most work was not created as a function and cannot be out in this file
